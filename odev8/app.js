44ae7abbb8bf62ea5753070f9f1f410b5ba98184
Tugay Tuna <tugaytuna@outlook.com.tr>
2023-05-23 21:37:59 +0300

    odev8 update

---
+++ odev8/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+let ejs = require("ejs");
+
+server = express();
+const port = 3005;
+const blog = { id: 1, title: "Blog title", description: "Blog description" };
+
+// server.use(express.static("public"));
+server.get("/", (req, res) => {
+  res.send(blog);
+  //   res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
+
+server.listen(port, () => {
+  console.log(`${port}. listening...`);
+});
